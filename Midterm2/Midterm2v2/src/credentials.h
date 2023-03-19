2fd6534a9827159700061368599240b6a4edb981
Dr. Vernon Cox <santedprojectmanagement@gmail.com>
2023-03-19 06:49:48 -0600

    update Midterm2v3 n it works fine

--- Midterm2/Midterm2v2/src/credentials.h
+++ Midterm2/Midterm2v2/src/credentials.h
@@ -1,4 +1,4 @@
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883        // use 1883 for SSL
 #define AIO_USERNAME    "santed7"	// replace with your Adafruit.io username
-#define AIO_KEY         "aio_fOWe76rLGJrwCNgKjdmXBYzxtVpW"		    // replace with your Adafruit.io key
+#define AIO_KEY         "aio_DSKm47QjQ7fOuZfv8wwiwB7N6HtN"		    // replace with your Adafruit.io key
